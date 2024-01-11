ae470dae22b7fc78c2faf259f604b229b6a9d531
Arghya <bhowmickarghyadeep2@gmail.com>
2024-01-11 13:17:35 +0530

    Global search ui implemented

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import React from "react";
 import Theme from "./Theme";
 import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
@@ -20,7 +21,9 @@ const Navbar = () => {
           Dev <span className="text-primary-500">OverFlow</span>
         </p>
       </Link>
-      GlobalSearch
+
+      <GlobalSearch />
+
       <div className="flex-between gap-5">
         <Theme />
 
