92c1ef0ef60958ebcd7e04412c0c580f30dfd741
Arghya <bhowmickarghyadeep2@gmail.com>
2024-02-04 16:54:24 +0530

    Tags page ui created

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import User from "@/database/user.model";
-import { GetTopInteractedTagsParams } from "./shared.types";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
 import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -22,6 +23,19 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "tag2" },
       { _id: "3", name: "tag3" },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+
+    const tags = await Tag.find({});
+
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
